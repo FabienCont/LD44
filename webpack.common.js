72269fd6545541d6ec6ca9b2c84ebbd3ba6d37c2
Contoli Fabien <contoli.fabien@gmail.com>
2019-04-27 11:43:40 +0200

    first commit

---
+++ webpack.common.js
@@ -0,0 +1,54 @@
+const path = require('path');
+
+module.exports = {
+
+    'entry': './sources/index.js',
+    'mode': 'none',
+    'module': {
+
+        'rules': [
+
+            {
+                'test': /\.js$/,
+                'use': [
+
+                    {
+                        'loader': 'babel-loader',
+                        'options': {
+
+                            'presets': [
+
+                                '@babel/preset-env'
+                            ]
+                        }
+                    }
+                ]
+            },
+            {
+                'test': /\.mp3|\.png$/,
+                'use': [
+
+                    {
+                        'loader': 'url-loader'
+                    }
+                ]
+            }
+        ]
+    },
+    'output': {
+
+        'path': path.resolve(__dirname, 'docs/'),
+        'filename': 'index.js'
+    },
+    'resolve': {
+
+        'alias': {
+
+            'assets': path.resolve(__dirname, 'sources/game/assets/'),
+            'scenes': path.resolve(__dirname, 'sources/game/scenes/'),
+
+            'core': path.resolve(__dirname, 'sources/theatre/core/'),
+            'modules': path.resolve(__dirname, 'sources/theatre/modules/')
+        }
+    }
+};
